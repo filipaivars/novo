6b69770e94372162e7ee01f95d35c43526f41297
filipaivars <ivars.filipa@gmail.com>
2016-03-28 05:10:38 +0100

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lbawApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lbawApp
+ */
+angular.module('lbawApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lbawApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lbawApp
+ */
+angular.module('lbawApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
